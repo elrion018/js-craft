6f752afb6360808094778be119bbae32e419b861
Daniel <elrion018@gmail.com>
2021-08-07 04:28:16 +0900

    [feature] 마우스 관련 이벤트 등록

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
 const gameFieldMethods = {
-  init: function init() {
-    const canvas = document.createElement('canvas')
-    this.ctx = canvas.getContext('2d')
-    canvas.className += 'field-canvas'
-    const app = document.getElementById('app')
+  init: function () {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.canvas.className += 'field-canvas';
+    const app = document.getElementById('app');
 
-    app.appendChild(canvas)
-    this.ctx.fillStyle = 'green'
+    app.appendChild(this.canvas);
+    this.ctx.fillStyle = 'green';
+
+    this.addListenersForMouseEvent();
   },
 
-  draw: function draw() {
+  addListenersForMouseEvent: function () {
+    this.canvas.addEventListener('mousemove', this.handleMouseMove);
+    this.canvas.addEventListener('mousedown', this.handleMouseDown);
+    this.canvas.addEventListener('mouseup', this.handleMouseUp);
+  },
+
+  handleMouseUp: function (event) {
+    console.log(event);
+  },
+
+  handleMouseDown: function (event) {
+    console.log(event);
+  },
+
+  handleMouseMove: function (event) {},
+
+  draw: function () {
     this.matrix.forEach((row, y) => {
       row.forEach((col, x) => {
         if (col === 0) {
-          this.ctx.fillRect(10 * x , 10 * y, 10 * (x + 1), 10 * (y + 1))
+          this.ctx.fillRect(10 * x, 10 * y, 10 * (x + 1), 10 * (y + 1));
         }
-      })
-    })
-  }
-}
+      });
+    });
+  },
+};
 
 const gameField = {
-  matrix: Array.from({length: 10}, () => Array.from({length: 10}, () => 0)),
-  ctx : null,
-}
+  matrix: Array.from({ length: 100 }, () =>
+    Array.from({ length: 100 }, () => 0)
+  ),
+  canvas: null,
+  ctx: null,
+  mouseIsPressed: false,
+};
 
-Object.setPrototypeOf(gameField, gameFieldMethods)
+Object.setPrototypeOf(gameField, gameFieldMethods);
 
-gameField.init()
-gameField.draw()
+gameField.init();
+gameField.draw();
