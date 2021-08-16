c1e8bb6bd0128f0362f83a70026305754b54b001
Daniel <elrion018@gmail.com>
2021-08-16 18:58:51 +0900

    [feature] 최소힙 구현

--- src/utils/minHeap.js
+++ src/utils/minHeap.js
@@ -14,4 +14,65 @@ export const minHeap = {
   getParentIndex: function (childIndex) {
     return Math.floor((childIndex - 1) / 2);
   },
+
+  insert: function (cost, value) {
+    const node = { cost, value };
+
+    this.heap.push(node);
+    this.heapifyUp();
+  },
+
+  heapifyUp: function () {
+    let index = this.heap.length - 1;
+    const lastInsertedNode = this.heap[index];
+
+    while (index > 0) {
+      const parentIndex = this.getParentIndex(index);
+
+      if (this.heap[parentIndex].cost > lastInsertedNode.cost) {
+        this.heap[index] = this.heap[parentIndex];
+        index = parentIndex;
+      } else break;
+    }
+
+    this.heap[index] = lastInsertedNode;
+  },
+
+  remove: function () {
+    const { length } = this.heap;
+    const rootNode = this.heap[0];
+
+    if (length <= 0) return null;
+    if (length === 1) this.heap = [];
+    else {
+      this.heap[0] = this.heap.pop();
+      this.heapifyDown();
+    }
+
+    return rootNode;
+  },
+
+  heapifyDown: function () {
+    let index = 0;
+    const { length } = this.heap;
+    const rootNode = this.heap[index];
+
+    while (this.getLeftChildIndex(index) < length) {
+      const leftChildIndex = this.getLeftChildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
+
+      const smallerChildIndex =
+        rightChildIndex < length &&
+        this.heap[rightChildIndex].cost < this.heap[leftChildIndex].cost
+          ? rightChildIndex
+          : leftChildIndex;
+
+      if (this.heap[smallerChildIndex].cost <= rootNode.cost) {
+        this.heap[index] = this.heap[smallerChildIndex];
+        index = smallerChildIndex;
+      } else break;
+    }
+
+    this.heap[index] = rootNode;
+  },
 };
